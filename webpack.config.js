2645984a65745da9f944dc53b8bef93eb805f458
Altas dallas <atlas.webdev89@gmail.com>
2022-08-03 16:10:39 +0300

    feat: new sting webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,47 @@
 // webpack.config.js
 const path = require('path');
+// путь к источнику 
+const pathSrc = path.resolve(__dirname, './app/');
+// путь к папке билда
+const pathDest= path.resolve(__dirname, './dist/');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // Очищает выходной каталог при каждом запуске сборщика webPack
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// Копирование файлов
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: "development",
     entry: {
-        "js/main.bundle.js": path.resolve(__dirname, './app/js/index.js'),
+        "js/main.bundle.js": pathSrc + '/js/index.js',
     },
     output: {
-        path: path.resolve(__dirname, './dist/'),
+        path: pathDest,
         filename: '[name]',
+    },
+    module: {
+        rules: [
+            // JavaScript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './app/index.html'), // шаблон
+            template: pathSrc + '/index.html', // шаблон
             inject: 'body',
             filename: 'index.html', // название выходного файла
         }),
         new CleanWebpackPlugin(),
+
+        new CopyPlugin({
+            patterns: [
+            { from: pathSrc +"/images", to: "images", },
+            { from: pathSrc + "/fonts", to: "fonts" },
+          ]
+        }),
     ],
 }
